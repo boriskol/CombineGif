4c2950886b5cfa168d2a029f9c7e72f6762345e1
borna libertines <2532576+boriskol@users.noreply.github.com>
2022-03-14 16:35:35 +0100

    Initial Commit

---
+++ Shared/CombineGifApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineGifApp.swift
+//  Shared
+//
+//  Created by Borna Libertines on 14/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CombineGifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
